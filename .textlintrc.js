1b5f2ed49531dd3017ab1bc920866e9dd89d5857
bwkw <na.shota.1139@gmail.com>
2023-08-29 04:53:21 -0700

    :gear: Install textlint

---
+++ .textlintrc.js
@@ -0,0 +1,15 @@
+// https://github.com/textlint/textlint/wiki/Collection-of-textlint-rule
+module.exports = {
+  plugins: {
+    "@textlint/markdown": {
+      extensions: [".md"],
+    },
+  },
+  rules: {
+    "preset-ja-technical-writing": true,
+    "preset-jtf-style": {
+      "2.1.6.カタカナの長音": true,
+      "2.2.1.ひらがなと漢字の使い分け": true,
+    },
+  },
+};
